9f6a284fc669115d920ea1bb81596a282ce6e0ca
dragonhn1106 <dragonhn1006@gmail.com>
2019-01-01 23:44:01 +0700

    upload 4 page: done

--- common/js/main.js
+++ common/js/main.js
@@ -2,3 +2,18 @@ function clickOpen() {
     var element = document.getElementById("bundle-entry-symbol1-placeholderss");
     element.classList.toggle("open");
 }
+
+function clickOpenRequest() {
+    var element = document.getElementById("bundle-entry-symbol1-placeholderss-Request");
+    element.classList.toggle("open");
+}
+
+function clickOpenCurrency() {
+    var element = document.getElementById("bundle-entry-symbol1-placeholderss-WITHDRAW");
+    element.classList.toggle("open");
+}
+
+function clickOpenTRANSFER() {
+    var element = document.getElementById("bundle-entry-symbol1-placeholderss-TRANSFER");
+    element.classList.toggle("open");
+}
